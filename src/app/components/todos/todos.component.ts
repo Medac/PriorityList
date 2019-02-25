20def33e494c7e41c240f27a8bece3910d06c8fc
Cade Marsh <Cadem9@gmail.com>
2019-02-25 15:07:38 -0600

    Adds initial Todo list setup

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './todo.service'
+
+import { Todo } from './todo'
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
+  todos:Todo[];
+
 
   ngOnInit() {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    })
   }
 
+  deleteTodo(todo:Todo) {
+    //Remove from UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    //Remove from Server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo:Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => { this.todos.push(todo);
+    });
+  }
 }
